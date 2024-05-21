9f6647aaced872a343c8935a25e585f60fcc5795
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-21 11:24:18 +0300

    add return type functions

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Profile extends Model
 {
@@ -16,7 +17,7 @@ class Profile extends Model
         'avatar',
     ];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id', 'id');
     }
