e2e69d1bb7584c1320f62b31d21fa386f8df33d8
Mohammed Al-Jarrah <moh.moh.aljarrah@gmail.com>
2024-05-21 10:40:43 +0300

    add one user

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('users/{id}', function ($id) {
+
+    $user = User::where('id', $id)->first();
+    dd($user->toArray());
 });
